27423486179bc015c773a50b3707e8dce19bf1e7
Michael Cammarere <MichaelCammarere@gmail.com>
2022-12-04 13:58:19 -0500

    Continuing to work towards tokenization.

---
+++ bow.py
@@ -0,0 +1,15 @@
+#using NLTK library, we can do lot of text preprocesing
+import nltk
+import time
+nltk.download('punkt')
+print("Hi this is planet explorer!\n")
+time.sleep(1)
+text = input("What question would you like to ask?\n\n")
+
+#function to split text into word
+tokens = nltk.tokenize.word_tokenize(text)
+nltk.download('stopwords')
+print(tokens)
+print(text)
+
+print("Thank you!")
