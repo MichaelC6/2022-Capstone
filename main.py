93e1648da72b422cccd3d03bacc636d95cabf69c
Michael Cammarere <MichaelCammarere@gmail.com>
2022-12-06 01:12:20 -0500

    Have it recognizing some entities, need to get it to output something now!

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #This is the main program that will run
 import time
-from entity_functions import load_entities
-from spacy.kb import KnowledgeBase
-import spacy
+import pandas
+import pandas as pd
+
+from nlp import extract_important_words
 
 print("Hi this is planet explorer!\n")
 time.sleep(1)
@@ -12,36 +13,17 @@ print(question)
 
 print("Thank you!")
 
-nlp = spacy.load("en_core_web_lg")
-text = "Home is around yellow thing, near sad depressing planet."
-doc = nlp(text)
-for ent in doc.ents:
-    print(f"Named Entity '{ent.text}' with label '{ent.label_}'")
 
-name_dict, desc_dict = load_entities()
-for QID in name_dict.keys():
-    print(f"{QID}, name={name_dict[QID]}, desc={desc_dict[QID]}")
+important_words = extract_important_words(question)
 
-kb = KnowledgeBase(vocab=nlp.vocab, entity_vector_length=300)
-
-for qid, desc in desc_dict.items():
-    desc_doc = nlp(desc)
-    desc_enc = desc_doc.vector
-    kb.add_entity(entity=qid, entity_vector=desc_enc, freq=342)   # 342 is an arbitrary value here
-
-for qid, name in name_dict.items():
-    kb.add_alias(alias=name, entities=[qid], probabilities=[1])   # 100% prior probability P(entity|alias)
-
-qids = name_dict.keys()
-probs = [0.3 for qid in qids]
-kb.add_alias(alias="Emerson", entities=qids, probabilities=probs)  # sum([probs]) should be <= 1 !
-
-print(f"Entities in the KB: {kb.get_entity_strings()}")
-print(f"Aliases in the KB: {kb.get_alias_strings()}")
+print(important_words)
 
 
-print(f"Candidates for 'Earth': {[c.entity_ for c in kb.get_alias_candidates('home')]}")
-print(f"Candidates for 'Sun': {[c.entity_ for c in kb.get_alias_candidates('yellow thing')]}")
-print(f"Candidates for 'Pluto': {[c.entity_ for c in kb.get_alias_candidates('depressing')]}")
+df = pd.read_csv("assets/planets.csv")
 
-#https://medium.com/analytics-vidhya/nlp-text-encoding-a-beginners-guide-fa332d715854
+planets = df["Planet"].tolist()
+planets = [planet.lower() for planet in planets]
+
+#need to match planet
+
+
--- v2.py
+++ v2.py
@@ -1,9 +1,8 @@
-#This python file deals with the NLP around my project.
 import nltk
 from nltk.corpus import stopwords
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-
+import pandas as pd
+from pathlib import Path
+nltk.download('averaged_perceptron_tagger')
 # first, define a function to preprocess the text by removing punctuation, stopwords, and lowercasing all words
 def preprocess(text):
     text = text.lower() # lowercase all words
@@ -12,7 +11,7 @@ def preprocess(text):
     filtered_tokens = [w for w in tokens if not w in stop_words] # remove stopwords
     return filtered_tokens
 
-# next, define a function to extract the most important words from a question using named entity recognition
+# next, define a function to extract the most important words from a question
 def extract_important_words(question):
     # preprocess the question to remove punctuation, stopwords, etc.
     tokens = preprocess(question)
@@ -25,11 +24,30 @@ def extract_important_words(question):
 
     # iterate through the tagged words and identify the "important" ones
     for word, pos in tags:
-        if pos in ["NN", "NNP", "JJ", "RB"]: # nouns and proper nouns are likely to be important
+        if pos in ["NN", "NNP"]: # nouns and proper nouns are likely to be important
             important_words.append(word)
         elif pos in ["VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]: # verbs are likely to indicate the action being taken
             important_words.append(word)
 
+    # identify adjectives and adverbs, which often provide important information
+    if pos in ["JJ", "JJR", "JJS", "RB", "RBR", "RBS"]:
+        important_words.append(word)
+
+    # identify prepositions and determiners, which often indicate the context of the sentence
+    if pos in ["IN", "DT"]:
+        important_words.append(word)
+
     # return the list of important words
     return important_words
 
+# now, let's test our function on some sample questions
+question = input("What is your question?\n")
+
+important_words = extract_important_words(question)
+
+# print the question and the important words
+print("Question:", question)
+print("Important words:", important_words)
+
+df = pd.read_csv("assets/planets.csv")
+print(df)
